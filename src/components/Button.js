67affb237ec8874d381627f3a42869e831901587
Korankye-Gyan <korankyefoster@gmail.com>
2023-03-08 20:11:26 +0000

    initial commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,17 +3,25 @@ import React,{useState} from 'react'
 const Button =() => {
   const initialCount = 0;
   const [count,setCount] = useState(initialCount);
+
+  const incrementFive = ()=>{
+    for (let i = 0; i< 5; i++){
+      setCount(prevCount=>prevCount +1)
+    }
+  }
+
   return (
     <div>
-      <h2 className='display'>Counter - {count}</h2>
+      <h2 className='display'>Counter: {count}</h2>
       <div className='btnContainer'>
          <button className='btn'onClick={()=>setCount(initialCount)}>Reset</button>
-         <button className='btn'onClick={()=>setCount(count +5)}>Increment</button>
-         <button className='btn'onClick={()=>setCount(count -5)}>Decrement</button>
+         <button className='btn'onClick={()=>setCount(count +1)}>Increment</button>
+         <button className='btn'onClick={()=>setCount(count -1)}>Decrement</button>
+         <button className='btn'onClick={incrementFive}>Increment Five</button>
       </div>
     </div>
     
-  )
+  ) 
 }
 
 export default Button
